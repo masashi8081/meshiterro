136b0fd6cc0f5f166b050c7be7fa55c3fbc7584c
masashi8081 <126481445+masashi8081@users.noreply.github.com>
2023-03-23 09:23:08 +0000

    3-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :postimages, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
