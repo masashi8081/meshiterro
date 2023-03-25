e8cf00638c3ff517d44cdb5f2a00bf57d7e901cf
masashi8081 <126481445+masashi8081@users.noreply.github.com>
2023-03-25 14:33:31 +0000

    3-18

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-
-  resources :post_images, only: [:new, :create, :index, :show]
-
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about', as: 'about'
-
   devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  get 'homes/about' => 'homes#about', as: 'about'
 
 
 end
